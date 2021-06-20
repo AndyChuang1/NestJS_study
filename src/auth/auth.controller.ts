4972c80c1d81a2d3837526c185979ffdb1e304bb
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2021-06-20 17:52:33 +0800

    feat(auth): Add jwt service to signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { SignIn } from './signin.model.entity';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -12,8 +13,9 @@ export class AuthController {
   signUp(@Body() authDto: AuthDto): Promise<void> {
     return this.authService.signUp(authDto);
   }
+
   @Post('/signin')
-  signIn(@Body() authDto: AuthDto): Promise<string> {
+  signIn(@Body() authDto: AuthDto): Promise<SignIn> {
     return this.authService.signIn(authDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,27 @@ import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private uesrsRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
   signUp(authDto: AuthDto): Promise<void> {
     return this.uesrsRepository.createUser(authDto);
   }
-  async signIn(authDto: AuthDto): Promise<string> {
+  async signIn(authDto: AuthDto): Promise<{ jwtToken: string }> {
     const { userName, password } = authDto;
     const user = await this.uesrsRepository.findOne({ userName });
     const validPwd = await bcrypt.compare(password, user.password);
 
     if (user && validPwd) {
-      return 'Success';
+      const payload = { userName };
+      const jwtToken = await this.jwtService.sign(payload);
+      return { jwtToken };
     } else {
       throw new UnauthorizedException('Please check your account or password');
     }
